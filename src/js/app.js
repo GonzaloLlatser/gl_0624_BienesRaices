64a7cdd1732d2674ecf073e53ce3180a2c5becc1
GonzaloLlatser <gonllat@gmail.com>
2024-07-01 17:32:11 +0200

    style: Crea botón DarkMode, se desarrolla función JS, aplican filtros y estilos. Se buscará aplicar a futuro un código PHP para evitar la modificación en cada vista por separado. Se comprueba la funcionalidad correcta

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,35 @@
-// Menu de Hamburguesa
-
 document.addEventListener("DOMContentLoaded", function () {
+  // Llamado a las Funciones
   eventListeners();
+  darkMode();
 });
 
 function eventListeners() {
+  // Selecciona elemento del DOM
   const mobileMenu = document.querySelector(".mobile-menu");
+  // Vincula llamado de funcion al elemento
   mobileMenu.addEventListener("click", navegacionResponsive);
 }
 
+// Menu de Hamburguesa
 function navegacionResponsive() {
+  // Selecciona elemento del DOM
   const navegacion = document.querySelector(".navegacion");
+  // Condicional = modificacion de clase y estilos CSS
   if (navegacion.classList.contains("mostrar")) {
     navegacion.classList.remove("mostrar");
   } else {
     navegacion.classList.add("mostrar");
   }
+}
+
+// Modo Oscuro o DarkMode
+function darkMode() {
+  // Selecciona elemento del DOM
+  const botonDarkMode = document.querySelector(".dark-mode-boton");
+  // Vincula llamado de funcion al elemento
+  botonDarkMode.addEventListener("click", function () {
+    // Condicional toggle para modificar clase del elemento
+    document.body.classList.toggle("dark-mode");
+  });
 }
