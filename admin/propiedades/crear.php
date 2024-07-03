a1f0d9c12f582d278d04176865cc220153b070e3
GonzaloLlatser <gonllat@gmail.com>
2024-07-03 12:24:24 +0200

    Genera la primera parte del CRUD, donde ejecuta la obtención de los datos del formulario, creación de la query y envío de la misma. Se observa correcta inserción de los datos en la BBDD. Se continuará con la implementación de la validación de los inputs

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -8,6 +8,35 @@ $db = conectarBD();
 require('../../includes/funciones.php');
 // Llama a la función incluirTemplate()
 incluirTemplate("header");
+
+// Almacenamiento de datos del Formulario
+// Validacion
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  // - Datos visuales p/ Comprobacion de Obtencion
+  // echo "<pre>";
+  // var_dump($_POST);
+  // echo "<pre>";
+  // - Almacena Datos en variables
+  $titulo = $_POST["titulo"];
+  $precio = $_POST["precio"];
+  $descipcion = $_POST["descipcion"];
+  $habitaciones = $_POST["habitaciones"];
+  $wc = $_POST["wc"];
+  $estacionamiento = $_POST["estacionamiento"];
+  $vendedorId = $_POST["vendedor"];
+  // Insertar en la BBDD
+  // - Genera Query SQL
+  $query = "INSERT INTO propiedades(titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedorId) VALUES('$titulo','$precio','$descipcion','$habitaciones','$wc','$estacionamiento','$vendedorId')";
+  // - Envia la query( Pasa como parametro la conexion $db, y la consulta $query)
+  $resultado = mysqli_query($db, $query);
+  // - Mensaje de Confirmacion
+  if ($resultado == true) {
+    echo "Insertado Correctamente";
+  } else {
+    echo "No se pudo insertar los datos";
+  }
+}
+
 ?>
 
 <main class="contenedor seccion">
@@ -15,30 +44,30 @@ incluirTemplate("header");
   <!-- Enlaces -->
   <a href="../index.php" class="boton boton-verde">Volver</a>
   <!-- Formulario -->
-  <form action="" class="formulario">
+  <form action="../../admin/propiedades/crear.php" class="formulario" method="POST">
     <fieldset>
       <legend>Información General</legend>
       <label for="titulo">Titulo:</label>
-      <input type="text" id="titulo" placeholder="Título Propiedad">
+      <input type="text" id="titulo" name="titulo" placeholder="Título Propiedad">
       <label for="precio">Precio:</label>
-      <input type="number" id="precio" placeholder="Precio Propiedad">
+      <input type="number" id="precio" name="precio" placeholder="Precio Propiedad">
       <label for="imagen">Imagen::</label>
       <input type="file" id="imagen" accept="image/jpeg, image/png">
       <label for="descipcion">Descripción:</label>
-      <textarea id="descripcion"></textarea>
+      <textarea id="descripcion" name="descipcion"></textarea>
     </fieldset>
     <fieldset>
       <legend>Información Propiedad</legend>
       <label for="habitaciones">Habitaciones:</label>
-      <input type="number" id="titulo" placeholder="Ejemplo: 3" min="1" max="9">
+      <input type="number" id="titulo" name="habitaciones" placeholder="Ejemplo: 3" min="1" max="9">
       <label for="baños">Baños:</label>
-      <input type="number" id="wc" placeholder="Ejemplo: 3" min="1" max="9">
+      <input type="number" id="wc" name="wc" placeholder="Ejemplo: 3" min="1" max="9">
       <label for="estacionamiento">Garage:</label>
-      <input type="number" id="estacionamiento" placeholder="Ejemplo: 1" min="0" max="5">
+      <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ejemplo: 1" min="0" max="5">
     </fieldset>
     <fieldset>
       <legend>Vendedor</legend>
-      <select>
+      <select name="vendedor">
         <option value="1">Juan</option>
         <option value="1">Karen</option>
       </select>
